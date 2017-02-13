9234d56c6865960d8a67f985c6a0d7fb708dd20f
Paul Mun <paulmun818@gmail.com>
2017-02-13 15:21:38 -0800

    functionality for Contact page and small portrait display styling complete

--- client/assets/js/controllers/contactController.js
+++ client/assets/js/controllers/contactController.js
@@ -5,5 +5,37 @@
 
     function contactController($scope, $location){
 
+        var serviceList = ['branding', 'digital experiences', 'social media', 'brand & product content', 'something else'];
+        $scope.services = '';
+        $scope.selectedServices = [false, false, false, false, false];
+
+        $scope.serviceClass = ['unselectedService', 'unselectedService', 'unselectedService', 'unselectedService', 'unselectedService'];
+
+        $scope.modal = false;
+
+        $scope.writeServices = function() {
+            var temp = [];
+
+            for (var i = 0; i < 5; i++) {
+                if($scope.selectedServices[i]) {
+                    temp.push(serviceList[i]);
+                }
+            }
+
+            $scope.services = temp.join(', ');
+
+            $scope.modal = false;
+        }
+
+        $scope.serviceClick = function(index) {
+            $scope.selectedServices[index] = !$scope.selectedServices[index];
+
+            if ($scope.selectedServices[index]) {
+                $scope.serviceClass[index] = 'selectedService';
+            } else {
+                $scope.serviceClass[index] = 'unselectedService';
+            }
+        }
+
     };
 })();
